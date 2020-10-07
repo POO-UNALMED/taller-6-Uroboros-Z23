4036e14d227a3c1905e6955c167a70f5c6a6577d
Daniel <69688189+Uroboros-Z23@users.noreply.github.com>
2020-10-06 22:32:48 -0500

    Update Camioneta.java

--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -3,7 +3,7 @@ package vehiculos;
 public class Camioneta extends Vehiculo{
     private boolean volco;
     private static int cantidadCa;
-    public Camioneta(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante frabricante, boolean volco) {
+    public Camioneta(String placa, int puertas,  String nombre, int precio, int peso, Fabricante fabricante, boolean volco) {
         super(placa,puertas,90,nombre,precio,peso,"4X4",frabricante);
         this.volco = volco;
         cantidadCa++;
