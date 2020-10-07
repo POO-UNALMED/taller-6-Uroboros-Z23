4e58fe3e83da5928317b5560abaf11208cd90e39
Uroboros-Z23 <69688189+Uroboros-Z23@users.noreply.github.com>
2020-10-06 19:32:04 -0500

    Actualizacion 3: Creaccion de atributos

--- src/main/java/vehiculo/Camion.java
+++ src/main/java/vehiculo/Camion.java
@@ -2,7 +2,7 @@ package vehiculo;
 
 public class Camion extends Vehiculo{
     private String ejes;
-    public Vehiculo(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante frabricante,String ejes){
+    public Camion(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante frabricante,String ejes){
         super(placa,2,80,nombre,precio,peso,"4X2",frabricante);
         this.ejes = ejes;
     }
--- src/main/java/vehiculo/Camioneta.java
+++ src/main/java/vehiculo/Camioneta.java
@@ -2,7 +2,7 @@ package vehiculo;
 
 public class Camioneta extends Vehiculo{
     private boolean volco;
-    public Vehiculo(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante frabricante, boolean volco) {
+    public Camioneta(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante frabricante, boolean volco) {
         super(placa,puertas,90,nombre,precio,peso,"4X4",frabricante);
         this.volco = volco;
     }
