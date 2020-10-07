8c1a9d47fd72d53654a064632207c2ce46731cd9
Uroboros-Z23 <69688189+Uroboros-Z23@users.noreply.github.com>
2020-10-06 19:27:59 -0500

    Actualizacion: Creacion de atributos

--- src/main/java/vehiculo/Camion.java
+++ src/main/java/vehiculo/Camion.java
@@ -5,6 +5,13 @@ public class Camion extends Vehiculo{
     public Vehiculo(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante frabricante,String ejes){
         super(placa,2,80,nombre,precio,peso,"4X2",frabricante);
         this.ejes = ejes;
-        
+    }
+
+    public String getEjes() {
+        return ejes;
+    }
+
+    public void setEjes(String ejes) {
+        this.ejes = ejes;
     }
 }
